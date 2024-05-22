5359c8d06626669b0283568c964ae09a4af6bde4
LY <eayetc@gmail.com>
2024-05-22 15:19:54 -0700

    improved unknown vintage display

--- controllers/types.js
+++ controllers/types.js
@@ -9,20 +9,20 @@ module.exports = {
     update,
     warn,
     delete: deleteType,
- }
+}
 
- async function create(req, res) {
+async function create(req, res) {
     req.body.name = req.body.name.trim()
     req.body.user = req.user._id
     try {
-          const type = await Type.create(req.body)
-          const wine = await Wine.findById(req.params.id)
-          wine.type = type._id
-          await wine.save()
-          res.redirect(`/wines/${wine._id}`)
+        const type = await Type.create(req.body)
+        const wine = await Wine.findById(req.params.id)
+        wine.type = type._id
+        await wine.save()
+        res.redirect(`/wines/${wine._id}`)
     } catch (err) {
-          console.log(err)
-          res.render(`/wines/${wine._id}/types`, { errorMsg: err.message })
+        console.log(err)
+        res.render(`/wines/${wine._id}/types`, { errorMsg: err.message })
     }
 }
 
@@ -32,7 +32,7 @@ async function show(req, res) {
     res.render('types/show', { title: type.name, type, wines })
 }
 
- async function showAt(req, res) {
+async function showAt(req, res) {
     const wine = await Wine.findById(req.params.id)
     const types = await Type.find({ 'user': req.user._id })
     res.render('types/showAt', {
@@ -65,12 +65,12 @@ async function update(req, res) {
 
 async function warn(req, res) {
     const type = await Type.findById(req.params.id)
-    res.render('types/warning', {title: 'Confirm Delete?', type})
+    res.render('types/warning', { title: 'Confirm Delete?', type })
 }
 
 async function deleteType(req, res) {
     const type = await Type.findById({ '_id': req.params.id })
     if (!type) return res.redirect('/wines/index')
-    await Type.deleteOne({ '_id': req.params.id})
+    await Type.deleteOne({ '_id': req.params.id })
     res.redirect('/wines/index')
 }
